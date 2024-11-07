775adc7dcab6c4c79db0f47bfb365fc29c0faa11
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-11-07 10:25:07 +0100

    Create TimeDisplayPlugin.js

---
+++ TimeDisplayPlugin.js
@@ -0,0 +1,14 @@
+// Plugin configuration, this is used in the administration when plugins are loaded
+var pluginConfig = {
+    name: 'Time Display Plugin',
+    version: '1.0',
+    author: 'Highpoint',
+    frontEndPath: 'TimeDisplay/timedisplay.js'
+}
+
+// Backend (server) changes can go here...
+
+// Don't change anything below here if you are making your own plugin
+module.exports = {
+    pluginConfig
+}
