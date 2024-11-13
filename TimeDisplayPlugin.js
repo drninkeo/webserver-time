cfade43218e4d25a7e5dd28f7c33f2daf198a4a3
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-11-13 08:11:44 +0100

    Update TimeDisplayPlugin.js

--- TimeDisplayPlugin.js
+++ TimeDisplayPlugin.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Time Display Plugin',
-    version: '2.5a',
+    version: '2.5b',
     author: 'Highpoint',
     frontEndPath: 'TimeDisplay/timedisplay.js'
 }
